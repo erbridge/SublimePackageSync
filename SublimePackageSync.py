17b64ec0b1eec4d5d530431c28ca2eca2cedf972
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2013-12-02 22:19:04 +0000

    Add a command to sync a specific package.

--- SublimePackageSync.py
+++ SublimePackageSync.py
@@ -5,51 +5,14 @@ import sublime
 import sublime_plugin
 
 
-class SublimePackageSyncAllCommand(sublime_plugin.ApplicationCommand):
+class SublimePackageSyncGitError(Exception):
+    pass
+
+
+class SublimePackageSyncCommand(sublime_plugin.ApplicationCommand):
 
     def __init__(self):
         self.settings = None
-
-    def run(self, autorun=False):
-        sublime.set_timeout_async(lambda: self.sync(autorun), 0)
-
-    def description(self):
-        # TODO: Add a description here.
-        return None
-
-    def sync(self, autorun):
-        print("[SublimePackageSync] Syncing all packages.")
-        packages_to_sync = self.get_setting("sync_repos")
-        installed_packages = os.listdir(sublime.packages_path())
-        for package_name in packages_to_sync:
-            try:
-                if autorun and package_name in self.get_setting("auto_sync_ignore"):
-                    continue
-                package = packages_to_sync.get(package_name)
-                remotes = package.get("remotes")
-                package_path = os.path.join(
-                    sublime.packages_path(), package_name)
-                if package_name not in installed_packages:
-                    print("[SublimePackageSync] Cloning " + package_name + ".")
-                    remotes = self.git_clone(remotes, package_path)
-                if self.is_git_repo(package_path):
-                    print("[SublimePackageSync] Updating " +
-                          package_name + ".")
-                    existing_remotes = self.git_remote_show(package_path)
-                    if not self.git_remotes_check(remotes, existing_remotes, package_path):
-                        print("[SublimePackageSync] ERROR - Mismatched remote." +
-                              " Check your remote settings.")
-                        return
-                    self.git_remotes_add(
-                        remotes, existing_remotes, package_path)
-                    self.git_fetch(package_path)
-                    self.git_checkout(
-                        package.get("object_to_checkout"), package_path)
-                    self.git_submodule_update(package_path)
-            except SublimePackageSyncGitError:
-                print(
-                    "[SublimePackageSync] ERROR - Git command failed. See output above.")
-        print("[SublimePackageSync] Done!")
 
     def get_setting(self, key):
         if self.settings is None:
@@ -57,15 +20,48 @@ class SublimePackageSyncAllCommand(sublime_plugin.ApplicationCommand):
                 "SublimePackageSync.sublime-settings")
         return self.settings.get(key)
 
+    def sync_package(self, package_name, package, installed_packages):
+        try:
+            remotes = package.get("remotes")
+            package_path = os.path.join(sublime.packages_path(), package_name)
+
+            if package_name not in installed_packages:
+                print("[SublimePackageSync] Cloning " + package_name + ".")
+                remotes = self.git_clone(remotes, package_path)
+
+            if not self.is_git_repo(package_path):
+                return
+
+            print("[SublimePackageSync] Updating " + package_name + ".")
+
+            existing_remotes = self.git_remote_show(package_path)
+            if not self.git_remotes_check(remotes, existing_remotes, package_path):
+                print("[SublimePackageSync] ERROR - Mismatched remote." +
+                      " Check your remote settings.")
+                return
+
+            self.git_remotes_add(remotes, existing_remotes, package_path)
+            self.git_fetch(package_path)
+            self.git_checkout(package.get("object_to_checkout"), package_path)
+            self.git_submodule_update(package_path)
+
+        except SublimePackageSyncGitError:
+            print(
+                "[SublimePackageSync] ERROR - Git command failed." +
+                " See output above.")
+
     def is_git_repo(self, cwd):
-        process = subprocess.Popen(["git", "rev-parse", "--git-dir"],
-                                   cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process = subprocess.Popen(
+            ["git", "rev-parse", "--git-dir"],
+            cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdoutdata, stderrdata = process.communicate()
         if process.returncode != 0:
             print(stdoutdata.decode("utf-8"), end="")
             print(stderrdata.decode("utf-8"), end="")
             raise SublimePackageSyncGitError
-        return cwd == os.path.dirname(os.path.join(cwd, stdoutdata.decode("utf-8").strip().split("\n")[0]))
+        return cwd == os.path.dirname(
+            os.path.join(
+                cwd, stdoutdata.decode("utf-8").strip().split("\n")[0]))
 
     def git_clone(self, remotes, path):
         if "origin" in remotes:
@@ -77,28 +73,38 @@ class SublimePackageSyncAllCommand(sublime_plugin.ApplicationCommand):
 
     def git_remote_show(self, cwd, *args):
         process = subprocess.Popen(
-            ["git", "remote", "show"] + [x for x in args], cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            ["git", "remote", "show"] + list(args),
+            cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdoutdata, stderrdata = process.communicate()
         if process.returncode != 0:
             print(stdoutdata.decode("utf-8"), end="")
             print(stderrdata.decode("utf-8"), end="")
             raise SublimePackageSyncGitError
+
         return stdoutdata.decode("utf-8").strip().split("\n")
 
     def git_remotes_check(self, remotes, existing_remotes, cwd):
         for remote_name in remotes:
-            if remote_name in existing_remotes:
-                remote_info = self.git_remote_show(cwd, "-n", remote_name)
-                for line in remote_info:
-                    if "Fetch URL:" in line:
-                        return line.split(" ")[-1].strip(".git") == remotes.get(remote_name).strip(".git")
+            if remote_name not in existing_remotes:
+                continue
+
+            remote_info = self.git_remote_show(cwd, "-n", remote_name)
+            for line in remote_info:
+                if "Fetch URL:" not in line:
+                    continue
+
+                return line.split(" ")[-1].strip(".git") == remotes.get(remote_name).strip(".git")
+
         return True
 
     def git_remotes_add(self, remotes, existing_remotes, cwd):
         for remote_name in remotes:
-            if remote_name not in existing_remotes:
-                self.run_git_command(
-                    ["remote", "add", remote_name, remotes.get(remote_name)], cwd=cwd)
+            if remote_name in existing_remotes:
+                continue
+
+            self.run_git_command(
+                ["remote", "add", remote_name, remotes.get(remote_name)],
+                cwd=cwd)
 
     def git_fetch(self, cwd):
         self.run_git_command(["fetch"], cwd=cwd)
@@ -118,13 +124,55 @@ class SublimePackageSyncAllCommand(sublime_plugin.ApplicationCommand):
         stdoutdata, stderrdata = process.communicate()
         if process.returncode == 0:
             return True
+
         print(stdoutdata.decode("utf-8"), end="")
         print(stderrdata.decode("utf-8"), end="")
         return False
 
 
-class SublimePackageSyncGitError(Exception):
-    pass
+class SublimePackageSyncAllCommand(SublimePackageSyncCommand):
+
+    def run(self, autorun=False):
+        sublime.set_timeout_async(lambda: self.sync(autorun), 0)
+
+    def description(self):
+        # TODO: Add a description here.
+        return None
+
+    def sync(self, autorun):
+        print("[SublimePackageSync] Syncing all packages.")
+        packages_to_sync = self.get_setting("sync_repos")
+        installed_packages = os.listdir(sublime.packages_path())
+        for package_name in packages_to_sync:
+            if autorun and package_name in self.get_setting("auto_sync_ignore"):
+                continue
+            self.sync_package(
+                package_name, packages_to_sync.get(package_name),
+                installed_packages)
+        print("[SublimePackageSync] Done!")
+
+
+class SublimePackageSyncSpecificCommand(SublimePackageSyncCommand):
+
+    def run(self):
+        synced_packages = self.get_setting("sync_repos")
+        synced_package_names = list(synced_packages.keys())
+        sublime.active_window().show_quick_panel(
+            synced_package_names,
+            lambda index: sublime.set_timeout_async(
+                lambda: self.sync(
+                    synced_package_names[index], synced_packages), 0))
+
+    def description(self):
+        # TODO: Add a description here.
+        return None
+
+    def sync(self, package_name, synced_packages):
+        print("[SublimePackageSync] Syncing " + package_name + ".")
+        self.sync_package(
+            package_name, synced_packages.get(package_name),
+            os.listdir(sublime.packages_path()))
+        print("[SublimePackageSync] Done!")
 
 
 def plugin_loaded():
